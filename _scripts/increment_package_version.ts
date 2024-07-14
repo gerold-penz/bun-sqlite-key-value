94a8be9b7beec2309c8ba1c53bc16d00b17050ca
Gerold Penz <gerold@halvar.at>
2024-07-14 19:17:12 +0200

    Write new version into "jsr.json"

--- _scripts/increment_package_version.ts
+++ _scripts/increment_package_version.ts
@@ -3,4 +3,13 @@ import { join } from "node:path"
 
 
 $.cwd(join(__dirname, ".."))
-await $`bun run increment_package_version`
+
+// Increment version
+let version = await $`bun run increment_package_version`.text()
+version = version.trim().substring(1)
+
+// Write new version into "jsr.json"
+const jsrFile = Bun.file("../jsr.json")
+const jsrData = await jsrFile.json()
+jsrData.version = version
+await Bun.write(jsrFile, JSON.stringify(jsrData, undefined, 2))
