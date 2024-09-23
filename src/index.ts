00a6428fa80a290a7d0f0840e15498643123894e
Gerold Penz <gerold@halvar.at>
2024-09-23 22:14:24 +0200

    API-Docs

--- src/index.ts
+++ src/index.ts
@@ -1192,7 +1192,7 @@ export class BunSqliteKeyValue {
      * Returns the length of the list stored at `key`.
      *
      * If `key` does not exist, it is interpreted as an empty list and 0 is returned.
-     * An error is returned when the value stored at `key` is not an array. --> `Array.isArray()`
+     * An error is returned when the value stored at `key` is not an array.
      *
      * Inspired by: https://www.dragonflydb.io/docs/command-reference/lists/llen
      *
