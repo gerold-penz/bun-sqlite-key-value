d530ae8e498b80fc2701387b25ad4351396b2eb0
Gerold Penz <gerold@halvar.at>
2025-01-13 12:50:43 +0100

    ...

--- _scripts/bun_run_incr_version.ts
+++ _scripts/bun_run_incr_version.ts
@@ -3,10 +3,6 @@ import { join } from "node:path"
 
 
 $.cwd(join(__dirname, ".."))
-$.env({
-    ...process.env,
-    "npm_config_registry": "https://registry.npmjs.org/"
-})
 
 // Increment version
 await $`bun run incr_version`.text()
--- _scripts/bun_run_publish_npm_beta.ts
+++ _scripts/bun_run_publish_npm_beta.ts
@@ -3,10 +3,6 @@ import { join } from "node:path"
 
 
 $.cwd(join(__dirname, ".."))
-$.env({
-    ...process.env,
-    "npm_config_registry": "https://registry.npmjs.org/"
-})
 
 try {
     await $`bun run npm:publish:beta`
