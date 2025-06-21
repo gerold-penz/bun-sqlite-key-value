23a5e8afe4348b9455a7cd53c295e03c96e5b32a
Gerold Penz <gerold@halvar.at>
2025-06-21 16:21:13 +0200

    Docusaurus Docs

--- docs_docusaurus/docusaurus.config.ts
+++ docs_docusaurus/docusaurus.config.ts
@@ -25,7 +25,7 @@ const config: Config = {
     organizationName: 'gerold-penz', // Usually your GitHub org/user name.
     projectName: 'bun-sqlite-key-value', // Usually your repo name.
 
-    onBrokenLinks: 'throw',
+    onBrokenLinks: 'log',
     onBrokenMarkdownLinks: 'warn',
 
     // Even if you don't use internationalization, you can use this field to set
